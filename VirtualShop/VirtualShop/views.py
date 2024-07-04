d644a8c422f2cd1cddd25bb392307ad011c2da2d
ItamarDuoc <it.carrasco@duocuc.cl>
2024-07-04 02:55:38 -0400

    Update views.py

--- VirtualShop/VirtualShop/views.py
+++ VirtualShop/VirtualShop/views.py
@@ -1,32 +1,16 @@
 from django.http import HttpResponse
 from django.template import Template, Context
 
-def buy(request):
-    doc = open("VirtualShop/TEMPLATES/buy_page.html")
-    plt = Template(doc.read())
-    doc.close()
-    ctx = Context()
-    pagina = plt.render(ctx)
-    return HttpResponse(pagina)
-
-def digimon(request):
-    doc = open("VirtualShop/TEMPLATES/digimon_page.html")
-    plt = Template(doc.read())
-    doc.close()
-    ctx = Context()
-    pagina = plt.render(ctx)
-    return HttpResponse(pagina)
-
-def evangelion(request):
-    doc = open("VirtualShop/TEMPLATES/evangelion_page.html")
-    plt = Template(doc.read())
-    doc.close()
-    ctx = Context()
-    pagina = plt.render(ctx)
-    return HttpResponse(pagina)
-
 def home(request):
     doc = open("VirtualShop/TEMPLATES/home_page.html")
+    plt = Template(doc.read())
+    doc.close()
+    ctx = Context()
+    pagina = plt.render(ctx)
+    return HttpResponse(pagina) # -> Esto debera 
+
+def buy(request):
+    doc = open("VirtualShop/TEMPLATES/buy_page.html")
     plt = Template(doc.read())
     doc.close()
     ctx = Context()
@@ -41,24 +25,8 @@ def login(request):
     pagina = plt.render(ctx)
     return HttpResponse(pagina)
 
-def pokemon(request):
-    doc = open("VirtualShop/TEMPLATES/pokemon_page.html")
-    plt = Template(doc.read())
-    doc.close()
-    ctx = Context()
-    pagina = plt.render(ctx)
-    return HttpResponse(pagina)
-
 def register(request):
     doc = open("VirtualShop/TEMPLATES/register_page.html")
-    plt = Template(doc.read())
-    doc.close()
-    ctx = Context()
-    pagina = plt.render(ctx)
-    return HttpResponse(pagina)
-
-def simpsons(request):
-    doc = open("VirtualShop/TEMPLATES/simpsons_page.html")
     plt = Template(doc.read())
     doc.close()
     ctx = Context()
